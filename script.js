7145af55fcaeecd88deb7363658eef268276fa80
flavin-jr <fjcvj@cin.ufpe.br>
2024-08-27 02:08:46 -0300

    feat: add only one button activate function

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const questionsContent = document.querySelectorAll('.question-content');
+const buttons = document.querySelectorAll('[plus]')
+console.log(buttons)
+
+const checkIsButtonActivate = (icon) => {
+
+    for (const button of buttons) {
+        if (button !== icon && !button.hasAttribute('plus')) {
+
+            button.parentNode.parentNode.parentNode.children[1].classList.toggle('hidden')
+            button.src = '/assets/images/icon-plus.svg';
+            button.toggleAttribute('plus')
+        }
+
+    }
+}
 
 const handleClick = (e, answer) => {
     const icon = e.target;
+
+    checkIsButtonActivate(icon);
+
     if (icon.hasAttribute('plus')) {
         icon.toggleAttribute('plus')
         icon.src = '/assets/images/icon-minus.svg';
